67e1ef26b7d43703eda31bcd0b369811140eca5a
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-02-16 00:58:35 -0300

    ¿Qué se implementó en ES8?

---
+++ src/ES8/Que-se-implemento-en-ES8/index.js
@@ -0,0 +1,15 @@
+const data = {
+    frontent: 'Oscar',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+const entries = Object.entries(data);
+// console.log(entries);
+// console.log(entries.length);
+
+const values = Object.values(data);
+// console.log(values);
+
+const string = 'Hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' ------'));
